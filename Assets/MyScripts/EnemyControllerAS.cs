c473e875eddb2323ee9a1fcbb1f65c2ee3dc8496
Fuji-Y <ukiuki0221kari@gmail.com>
2019-03-22 17:48:24 +0900

    RandomRPG 2回目
    テキストの表示がうまくいかない

--- Assets/MyScripts/EnemyControllerAS.cs
+++ Assets/MyScripts/EnemyControllerAS.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyControllerAS : MonoBehaviour
 {
@@ -11,12 +12,16 @@ public class EnemyControllerAS : MonoBehaviour
 
     BattleManager battleManager;
     UnityChanControllerAS unityChanControllerAS;
+    //表示するテキスト
+    private GameObject parameterText;
 
     // Start is called before the first frame update
     void Start()
     {
         battleManager = GameObject.Find("BattleManager").GetComponent<BattleManager>();
         unityChanControllerAS = GameObject.Find("unitychan").GetComponent<UnityChanControllerAS>();
+
+        this.parameterText = GameObject.Find("ParameterText");
 
         eneHP = 500;
         eneMP = 30;
@@ -39,16 +44,19 @@ public class EnemyControllerAS : MonoBehaviour
         if (damage <= 3)
         {
             unityChanControllerAS.myHP -= (eneATK - unityChanControllerAS.myDEF);
+            this.parameterText.GetComponent<Text>().text = "HPを" + (eneATK - unityChanControllerAS.myDEF) + "削られた！";
             battleManager.isMyturn = true;
         }
         else if (damage <= 6 && eneMP >=10)
         {
             eneMP -= 10;
             unityChanControllerAS.myHP -= eneATK * 1.2;
+            this.parameterText.GetComponent<Text>().text = "HPを" + (eneATK * 1.2) + "削られた！";
             battleManager.isMyturn = true;
         }
         else if (damage <= 9 && eneHP <= unityChanControllerAS.myATK)
         {
+            this.parameterText.GetComponent<Text>().text = "敵のHPが " + (eneHP * 0.3) + "回復\n" + "敵のMPが " + (eneMP * 0.3) + "回復";
             eneHP *= 1.3;
             eneMP *= 1.3;
             battleManager.isMyturn = true;
@@ -58,7 +66,12 @@ public class EnemyControllerAS : MonoBehaviour
             int kakuritu = Random.Range(1, 11);
             if (kakuritu <= 5)
             {
-                unityChanControllerAS.myHP -= eneATK * 3;
+                unityChanControllerAS.myHP -= (eneATK * 3 - unityChanControllerAS.myDEF);
+                this.parameterText.GetComponent<Text>().text = "HPを" + (eneATK * 3 - unityChanControllerAS.myDEF) + "削られた！";
+            }
+            else
+            {
+                this.parameterText.GetComponent<Text>().text = "敵が空振りした！";
             }
             battleManager.isMyturn = true;
         }
@@ -74,6 +87,10 @@ public class EnemyControllerAS : MonoBehaviour
             if (unityChanControllerAS.myHP > 0 && eneHP > 0)
             {
                 Attack();
+            }
+            else
+            {
+                this.parameterText.GetComponent<Text>().text = "戦闘終了:勝利！";
             }
         }
     }
--- Assets/MyScripts/UnityChanControllerAS.cs
+++ Assets/MyScripts/UnityChanControllerAS.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnityChanControllerAS : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class UnityChanControllerAS : MonoBehaviour
     public double myMP = 30;
     public int myDEF = 0;
     public int myATK = 100;
+    //表示するテキスト
+    private GameObject parameterText;
 
     BattleManager battleManager;
     EnemyControllerAS enemyControllerAS;
@@ -23,26 +26,33 @@ public class UnityChanControllerAS : MonoBehaviour
         enemyControllerAS = GameObject.Find("EnemyControllerAS").GetComponent<EnemyControllerAS>();
         
         animator = GetComponent<Animator>();
+        //シーン中のTextオブジェクトを取得
+        this.parameterText = GameObject.Find("ParameterText");
     }
 
     void Attack()
     {
+        this.parameterText.GetComponent<Text>().text = "1,2,3,4から数字を選択してください\n1.通常攻撃    2.魔法攻撃\n3.瞑想    4.必殺技";
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             enemyControllerAS.eneHP -= (myATK - enemyControllerAS.eneDEF);
             animator.SetBool("Jab", true);
+            this.parameterText.GetComponent<Text>().text = "HPを"+ (myATK - enemyControllerAS.eneDEF) + "削った！" ;
             battleManager.isMyturn = false;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2) && myMP >= 10)
         {
             myMP -= 10;
             enemyControllerAS.eneHP -= myATK * 1.2;
+            this.parameterText.GetComponent<Text>().text = "HPを" + (myATK * 1.2) + "削った！";
             battleManager.isMyturn = false;
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            this.parameterText.GetComponent<Text>().text = "HPが" + (myHP * 0.3) + "回復\n" + "MPが" + (myMP * 0.3) + "回復";
             myHP *= 1.3;
             myMP *= 1.3;
+            
             battleManager.isMyturn = false;
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
@@ -50,11 +60,21 @@ public class UnityChanControllerAS : MonoBehaviour
             int kakuritu = Random.Range(1, 11);
             if (kakuritu <= 5)
             {
-                enemyControllerAS.eneHP -= myATK * 3;
+                enemyControllerAS.eneHP -= (myATK * 3 - enemyControllerAS.eneDEF);
+                this.parameterText.GetComponent<Text>().text = "HPを" + (myATK * 3 - enemyControllerAS.eneDEF) + "削った！";
+            } else
+            {
+                this.parameterText.GetComponent<Text>().text = "空振りした！";
             }
+            
             battleManager.isMyturn = false;
         }
         //Debug.Log(hisHP);
+    }
+
+    public void EndAttack ()
+    {
+        animator.SetBool("Jab", false);
     }
     
 
@@ -67,6 +87,10 @@ public class UnityChanControllerAS : MonoBehaviour
             if (myHP > 0 && enemyControllerAS.eneHP > 0)
             {
                 Attack();
+            }
+            else
+            {
+                this.parameterText.GetComponent<Text>().text = "戦闘終了:敗北";
             }
         }
     }
